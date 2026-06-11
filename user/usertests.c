b99b24e57bfad01d6ae0b19db49147a0aea263a0
vikirna <vikirna@proton.me>
2026-06-11 20:33:17 +0545

    comment out fsblocks in usertests since it was not used

--- user/usertests.c
+++ user/usertests.c
@@ -2349,7 +2349,7 @@ void
 fsfull()
 {
   int nfiles;
-  int fsblocks = 0;
+  // int fsblocks = 0;
 
   printf("fsfull test\n");
 
@@ -2373,7 +2373,7 @@ fsfull()
       if (cc < BSIZE)
         break;
       total += cc;
-      fsblocks++;
+      // fsblocks++;
     }
     printf("wrote %d bytes\n", total);
     close(fd);
